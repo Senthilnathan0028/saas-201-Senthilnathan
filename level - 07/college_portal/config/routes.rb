d42da68e6879ee79db13e51aad53d1eb3e87d56b
Senthilnathan <senthilsenthil0028@gmail.com>
2019-10-10 15:25:13 +0530

    level-07

--- level - 07/college_portal/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static_pages#index"
-  resources :departments, only: [:index, :create, :new]
-  # resources :sections, only: [:index, :new, :create]
-  resources :sections, only: [:index, :new, :create]
-  resources :students, only: [:index, :new, :create, :show]
-
-end
